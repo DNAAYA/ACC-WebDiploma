333899d117139bc0f04f14d778d318ee03e2dd4c
Mahmoud Abotaleb <mabotaleb@outlook.com>
2020-02-05 10:56:05 +0200

    6-2

--- section 3-inclas/index.js
+++ section 3-inclas/index.js
@@ -1,13 +1,90 @@
-var age = "abdo"
+var name = 'Mahmoud';
+console.log(name);
+var number = 24;
+console.log(number);
+var number1 = 2;
+var number2 = 2;
+console.log(number1 == number2);    
+console.log('my name is ' , name);  
 
-if (age < 12) {
-    console.log("kid")
-} else if (age >= 12 && age < 20) {
-    console.log("teenager")
-} else if (age >= 20 && age < 45) {
-    console.log("man")
-} else if (age >= 45) {
-    console.log("old")
-} else {
-    alert("must be number")
+var name, age, job;
+name = 'Ahmed';
+age = 22;
+job = 'teacher';
+console.log(name , age , job);
+var noValue;
+
+console.log(noValue);
+
+var testNull = null; 
+console.log(testNull);
+
+console.log(typeof name); 
+console.log(typeof number);
+console.log ( typeof noValue);
+console.log(typeof testNull);
+
+var x = 2;
+x += 10;
+x -= 5;
+
+ x++;
+ x--;
+ 
+ // 1 - and =  &&
+ // 2 - or = ||
+ // 3 - not = ! 
+
+ var a = 5; 
+ var b = 6; 
+ console.log( a > 10 && b < 10);
+ console.log( a > 10 || b <10);
+ console.log( a == b);
+ console.log(!(a == b));
+
+ var firstName = 'abdo'; 
+ var age = 19; 
+ if (age < 19 ) {
+     console.log(firstName, 'is too young')
+ } 
+  else if ( age === 19 ) {  
+      console.log(firstName, 'young man')
+  } 
+
+    else { 
+         console.log(firstName, 'is old enough')
+    };
+
+
+switch (true) {
+    case age < 19: 
+    console.log(firstName, 'is too young');
+    break;
+    case age === 19:
+    console.log(firstName, 'young man');
+    break;
+    default: 
+    console.log(firstName, 'is old enough');
+};
+
+var job = 'teacher';
+switch (job) {
+    case 'teacher': 
+    console.log('teaches kids how to code.');
+    break;
+    case 'designer':
+    console.log('designs beautiful websites.');
+    break;
+    default:
+    console.log('do somthing else')
+}
+
+// ternary
+age < 20 ? console.log("too young") : console.log("old");
+
+if (age < 20) {
+    console.log("too young");
+}
+else {
+    console.log("old");
 }
---
+++ section 5/basics3.js
@@ -0,0 +1,103 @@
+
+
+/*****************************
+* Loops and iteration
+*/
+
+
+// for loop
+for (var i = 1; i <= 20; i += 2) {
+    console.log(i);
+}
+
+
+var john = ['John', 'Smith', 1990, 'designer', false, 'blue'];
+for (var i = 0; i < john.length; i++) {
+    console.log(john[i]);
+}
+
+// While loop
+var i = 0;
+while(i < john.length) {
+    console.log(john[i]);
+    i++;
+}
+
+
+// continue and break statements
+var john = ['John', 'Smith', 1990, 'designer', false, 'blue'];
+
+for (var i = 0; i < john.length; i++) {
+    if (typeof john[i] !== 'string') continue;
+    console.log(john[i]);
+}
+
+for (var i = 0; i < john.length; i++) {
+    if (typeof john[i] !== 'string') break;
+    console.log(john[i]);
+}
+
+// Looping backwards
+for (var i = john.length - 1; i >= 0; i--) {
+    console.log(john[i]);
+}
+
+
+
+/////////////////////////////////////
+//Scoping
+
+
+// First scoping example
+var a = 'Hello!';
+first();
+
+function first() {
+    var b = 'Hi!';
+    second();
+
+    function second() {
+        var c = 'Hey!';
+        console.log(a + b + c);
+    }
+}
+
+
+/////////////////////////////////////
+//The this keyword
+
+
+//console.log(this);
+
+calculateAge(1985);
+
+function calculateAge(year) {
+    console.log(2016 - year);
+    console.log(this);
+}
+
+var john = {
+    name: 'John',
+    yearOfBirth: 1990,
+    calculateAge: function() {
+        console.log(this);
+        console.log(2016 - this.yearOfBirth);
+        
+        function innerFunction() {
+            console.log(this);
+        }
+        innerFunction();
+    }
+}
+
+john.calculateAge();
+
+var mike = {
+    name: 'Mike',
+    yearOfBirth: 1984
+};
+
+
+mike.calculateAge = john.calculateAge;
+mike.calculateAge();
+
