dec21c48d59de5782581e82ceb0b776b918524e4
Abd Elrazik Hassan <57398787+ahassana@users.noreply.github.com>
2020-02-06 10:01:57 +0200

    Update index.js

--- Section 4-inclass/index.js
+++ Section 4-inclass/index.js
@@ -88,3 +88,68 @@ if (age < 20) {
 else {
     console.log("old");
 }
+
+
+/*****************************
+* CODING CHALLENGE 1
+*/
+
+/*
+Mark and John are trying to compare their BMI (Body Mass Index), 
+which is calculated using 
+the formula: BMI = mass / height^2 = mass / (height * height). (mass in kg and height in meter).
+
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs
+3. Create a boolean variable containing information about whether Mark has a higher
+ BMI than John.
+4. Print a string to the console containing the variable from step 3. (Something like "Is Mark's BMI higher than John's? true"). 
+
+GOOD LUCK 😀
+*/
+
+
+var massMark = 78; // kg
+var heightMark = 1.69; // meters
+
+var massJohn = 92;
+var heightJohn = 1.95;
+
+var BMIMark = massMark / (heightMark * heightMark);
+var BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log(BMIMark, BMIJohn);
+
+var markHigherBMI = BMIMark > BMIJohn;
+console.log('Is Mark\'s BMI higher than John\'s? ' + markHigherBMI);
+
+
+/*****************************
+* CODING CHALLENGE 2
+*/
+
+/*
+John and Mike both play basketball in different teams. 
+In the latest 3 games, John's team scored 89, 120 and 103 points, 
+while Mike's team scored 116, 94 and 123 points.
+
+1. Calculate the average score for each team
+2. Decide which teams wins in average (highest average score), 
+and print the winner to the console. Also include the average score in the output. (if statment)
+
+GOOD LUCK 😀
+*/
+
+
+
+var scoreJohn = (189 + 120 + 103) / 3;
+var scoreMike = (129 + 94 + 123) / 3;
+
+console.log(scoreJohn, scoreMike);
+
+if (scoreJohn > scoreMike) {
+    console.log('John\'s team wins with ' + scoreJohn + ' points');
+} else if (scoreMike > scoreJohn) {
+    console.log('Mike\'s team wins with ' + scoreMike + ' points');
+} else {
+    console.log('There is a draw');
+}
